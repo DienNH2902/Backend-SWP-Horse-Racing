3f95a9815af7567491ec316bbd27057885212e10
DienNH2902 <diennguyenhoang2902@gmail.com>
2026-07-23 16:09:41 +0700

    fix: fix filter withdraw management

--- src/modules/payment/withdrawal.repository.ts
+++ src/modules/payment/withdrawal.repository.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { ClientSession, Model, Types } from 'mongoose';
+import { ClientSession, Model, QueryFilter, Types } from 'mongoose';
 import {
   WithdrawalRequest,
   WithdrawalRequestDocument,
 } from './schemas/withdrawal.schema';
+import { User, UserDocument } from '../user/schemas/user.schema';
+import { WithdrawalStatusEnum } from 'src/constants/withdrawalStatusEnum.enum';
 
 @Injectable()
 export class WithdrawalRepository {
   constructor(
     @InjectModel(WithdrawalRequest.name)
     private readonly withdrawalModel: Model<WithdrawalRequestDocument>,
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
   ) {}
 
   async create(
@@ -46,9 +50,46 @@ export class WithdrawalRepository {
       .exec();
   }
 
-  async findAllRequests(): Promise<WithdrawalRequestDocument[]> {
+  // async findAllRequests(): Promise<WithdrawalRequestDocument[]> {
+  //   return this.withdrawalModel
+  //     .find()
+  //     .select('-__v')
+  //     .populate('userId', 'fullName email role')
+  //     .sort({ createdAt: -1 })
+  //     .lean()
+  //     .exec();
+  // }
+
+  async findAllRequests(
+    status?: WithdrawalStatusEnum,
+    search?: string,
+  ): Promise<WithdrawalRequestDocument[]> {
+    const filter: QueryFilter<WithdrawalRequestDocument> = {};
+
+    // Filter theo status nếu có
+    if (status) {
+      filter.status = status;
+    }
+
+    // Search theo fullName nếu có
+    if (search && search.trim()) {
+      const keyword = search.trim();
+
+      // Tìm danh sách userId có fullName chứa từ khóa search
+      const matchingUsers = await this.userModel
+        .find({ fullName: { $regex: keyword, $options: 'i' } })
+        .select('_id')
+        .lean()
+        .exec();
+
+      const matchedUserIds = matchingUsers.map((user) => user._id);
+
+      // Thêm điều kiện $in vào filter
+      filter.userId = { $in: matchedUserIds };
+    }
+
     return this.withdrawalModel
-      .find()
+      .find(filter)
       .select('-__v')
       .populate('userId', 'fullName email role')
       .sort({ createdAt: -1 })
